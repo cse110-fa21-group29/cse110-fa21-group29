19f2272dc00f4f3006b26f5377c2f38bb21df807
DFC <frankdoufc@gmail.com>
2021-11-10 13:23:10 -0800

    feat(home-pate): basic structure

---
+++ source/components/home-page/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.recipe-card-grid').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow: $(".next-button"),
+        prevArrow: $(".prev-button")
+    });
+});
