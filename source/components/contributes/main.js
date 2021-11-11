016b7cf01eac21c86e92e5697552a641e1ccac7a
DFC <frankdoufc@gmail.com>
2021-11-11 13:27:56 -0800

    Revert "feat: same"
    This reverts commit 50a9eca6374e0e69100c16142b7a798ce412ec56.

--- source/components/contributes/main.js
+++
@@ -1,5 +0,0 @@
-// change background img of element
-function readURL(tar, elementToChange) {
-    console.log( elementToChange)
-    document.getElementById(elementToChange).style.backgroundImage = "url("+URL.createObjectURL(tar.files[0])+")";
-}
