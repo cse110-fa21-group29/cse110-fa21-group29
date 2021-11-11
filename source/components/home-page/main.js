985ea71742f8fdb672d7c0946dcbf356bb1a4e69
DFC <frankdoufc@gmail.com>
2021-11-10 20:51:19 -0800

    feat(home-page): first draft

--- source/components/home-page/main.js
+++ source/components/home-page/main.js
@@ -1,10 +1,29 @@
+let x = []
+for (let i = 1; i < 20; i++) {
+    x[i] = {
+        breakpoint: 400 * i,
+        settings: {
+            slidesToShow: i,
+            slidesToScroll: i,
+            infinite: true,
+        }
+    }
+}
 $(document).ready(function () {
-    $('.recipe-card-grid').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        nextArrow: $(".next-button"),
-        prevArrow: $(".prev-button"),
-
+    $('.recipe-card-grid-1').slick({
+        nextArrow: $(".next-button-1"),
+        prevArrow: $(".prev-button-1"),
+        responsive: x
+    });
+    $('.recipe-card-grid-2').slick({
+        nextArrow: $(".next-button-2"),
+        prevArrow: $(".prev-button-2"),
+        responsive: x
+    });
+    $('.recipe-card-grid-3').slick({
+        nextArrow: $(".next-button-3"),
+        prevArrow: $(".prev-button-3"),
+        responsive: x
     });
 });
+
