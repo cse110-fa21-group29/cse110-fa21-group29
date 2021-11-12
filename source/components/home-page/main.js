027431257fff1901b92c24e1474d93c3f14b2e43
DFC <frankdoufc@gmail.com>
2021-11-11 18:31:17 -0800

    bug(homepage): solve Justin's change request

--- source/components/home-page/main.js
+++ source/components/home-page/main.js
@@ -1,8 +1,8 @@
 // used for build horizontal scroll recipes
 // https://kenwheeler.github.io/slick/
-let x = []
+let responsivelist = []
 for (let i = 1; i < 20; i++) {
-    x[i] = {
+    responsivelist[i] = {
         breakpoint: 450 * i,
         settings: {
             slidesToShow: i,
@@ -12,20 +12,19 @@ for (let i = 1; i < 20; i++) {
     }
 }
 $(document).ready(function () {
-    $('.recipe-card-grid-1').slick({
-        nextArrow: $(".next-button-1"),
-        prevArrow: $(".prev-button-1"),
-        responsive: x
+    $('#recipe-card-grid-1').slick({
+        nextArrow: $("#next-button-1"),
+        prevArrow: $("#prev-button-1"),
+        responsive: responsivelist
     });
-    $('.recipe-card-grid-2').slick({
-        nextArrow: $(".next-button-2"),
-        prevArrow: $(".prev-button-2"),
-        responsive: x
+    $('#recipe-card-grid-2').slick({
+        nextArrow: $("#next-button-2"),
+        prevArrow: $("#prev-button-2"),
+        responsive: responsivelist
     });
-    $('.recipe-card-grid-3').slick({
-        nextArrow: $(".next-button-3"),
-        prevArrow: $(".prev-button-3"),
-        responsive: x
+    $('#recipe-card-grid-3').slick({
+        nextArrow: $("#next-button-3"),
+        prevArrow: $("#prev-button-3"),
+        responsive: responsivelist
     });
 });
-
