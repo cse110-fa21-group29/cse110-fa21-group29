350c14987e90b704221ddf55de080f6408511c75
DFC <frankdoufc@gmail.com>
2021-11-13 11:43:15 -0800

    feat(homepage): scroll example

--- source/components/home-page/main.js
+++ source/components/home-page/main.js
@@ -1,30 +1,9 @@
-// used for build horizontal scroll recipes
-// https://kenwheeler.github.io/slick/
-let responsivelist = [];
-for (let i = 1; i < 20; i++) {
-  responsivelist[i] = {
-    breakpoint: 450 * i,
-    settings: {
-      slidesToShow: i,
-      slidesToScroll: i,
-      infinite: true,
-    },
-  };
-}
-$(document).ready(function () {
-  $("#recipe-card-grid-1").slick({
-    nextArrow: $("#next-button-1"),
-    prevArrow: $("#prev-button-1"),
-    responsive: responsivelist,
-  });
-  $("#recipe-card-grid-2").slick({
-    nextArrow: $("#next-button-2"),
-    prevArrow: $("#prev-button-2"),
-    responsive: responsivelist,
-  });
-  $("#recipe-card-grid-3").slick({
-    nextArrow: $("#next-button-3"),
-    prevArrow: $("#prev-button-3"),
-    responsive: responsivelist,
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("next-button-1").addEventListener("click", () => {
+    let recipecardgrid = document.getElementById("recipe-card-grid-1");
+    recipecardgrid.scroll({
+      left: recipecardgrid.clientWidth + recipecardgrid.scrollLeft,
+      behavior: "smooth",
+    });
   });
 });
