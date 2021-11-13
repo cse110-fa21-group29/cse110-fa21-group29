c44811c72fabd660a1045e90bec85e7fd2ea86ea
DFC <frankdoufc@gmail.com>
2021-11-13 12:15:05 -0800

    feat(homepage): replace slick

--- source/components/home-page/main.js
+++ source/components/home-page/main.js
@@ -1,9 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("next-button-1").addEventListener("click", () => {
-    let recipecardgrid = document.getElementById("recipe-card-grid-1");
-    recipecardgrid.scroll({
-      left: recipecardgrid.clientWidth + recipecardgrid.scrollLeft,
+  for (let i = 1; i < 4; i++) {
+    let recippecardgrid = document.getElementById("recipe-card-grid-" + i);
+    document
+      .getElementById("prev-button-" + i)
+      .addEventListener("click", () => {
+        recipeScroll(true, recippecardgrid);
+      });
+    document
+      .getElementById("next-button-" + i)
+      .addEventListener("click", () => {
+        recipeScroll(false, recippecardgrid);
+      });
+  }
+});
+
+function recipeScroll(scrollleft, recipegrid) {
+  if (scrollleft) {
+    recipegrid.scroll({
+      left: recipegrid.scrollLeft - recipegrid.clientWidth,
       behavior: "smooth",
     });
-  });
-});
+  } else {
+    recipegrid.scroll({
+      left: recipegrid.scrollLeft + recipegrid.clientWidth,
+      behavior: "smooth",
+    });
+  }
+}
