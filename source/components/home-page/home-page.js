0bf3c47fa1ca1c58f684ebf3f94e02d47db9ea89
Justin Butera <jbutera@ucsd.edu>
2021-11-15 00:54:18 -0800

    feat(home-page): migrate home page code to new file structure (16)

--- source/components/home-page/home-page.js
+++ source/components/home-page/home-page.js
@@ -1,3 +1,5 @@
+//import "@fortawesome/fontawesome-free/js/all.js";
+
 class HomePage extends HTMLElement {
   constructor() {
     super();
@@ -8,19 +10,55 @@ class HomePage extends HTMLElement {
     let elementContent = await fetch("components/home-page/home-page.html");
     let elementContentText = await elementContent.text();
     this.shadowRoot.innerHTML = elementContentText;
-    this.bindButton();
+    this.setupElement();
+    console.log("connected callback");
   }
 
-  bindButton() {
-    const buttonElement = this.shadowRoot.getElementById("button");
-    buttonElement.addEventListener("click", () => {
-      const routerEvent = new CustomEvent("router-navigate", {
-        detail: "recipe-details",
-        bubbles: true,
-        composed: true,
+  setupElement() {
+    console.log("setup element");
+    for (let i = 1; i < 4; i++) {
+      let recippecardgrid = this.shadowRoot.getElementById(
+        "recipe-card-grid-" + i
+      );
+      this.shadowRoot
+        .getElementById("prev-button-" + i)
+        .addEventListener("click", () => {
+          this.recipeScroll(true, recippecardgrid);
+        });
+      this.shadowRoot
+        .getElementById("next-button-" + i)
+        .addEventListener("click", () => {
+          this.recipeScroll(false, recippecardgrid);
+        });
+    }
+
+    // copy recipe
+    let recipesample = this.shadowRoot.getElementById("recipe-card-sample");
+    for (let i = 0; i < 20; i++) {
+      this.shadowRoot
+        .getElementById("recipe-card-grid-1")
+        .append(recipesample.cloneNode(true));
+      this.shadowRoot
+        .getElementById("recipe-card-grid-2")
+        .append(recipesample.cloneNode(true));
+      this.shadowRoot
+        .getElementById("recipe-card-grid-3")
+        .append(recipesample.cloneNode(true));
+    }
+  }
+
+  recipeScroll(scrollleft, recipegrid) {
+    if (scrollleft) {
+      recipegrid.scroll({
+        left: recipegrid.scrollLeft - (recipegrid.clientWidth * 3) / 4,
+        behavior: "smooth",
       });
-      buttonElement.dispatchEvent(routerEvent);
-    });
+    } else {
+      recipegrid.scroll({
+        left: recipegrid.scrollLeft + (recipegrid.clientWidth * 3) / 4,
+        behavior: "smooth",
+      });
+    }
   }
 }
 
