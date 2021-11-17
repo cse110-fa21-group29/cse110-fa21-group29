ae616d8b9b388438abbf401cf58d95b6d7f3e27f
DFC <frankdoufc@gmail.com>
2021-11-17 13:01:13 -0800

    feat(mealplanner): style chagne

---
+++ source/components/meal-planner/meal-planner.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let samplemeal = document.getElementById("sample-meal");
+  samplemeal.style.backgroundImage =
+    "url(../../static/home-page/demorecipe.jpg)";
+  samplemeal.style.backgroundSize = "cover";
+});
