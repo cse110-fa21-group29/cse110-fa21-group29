f66cc47a2a86dba23983a8be19c26835f31aa5f7
DFC <frankdoufc@gmail.com>
2021-11-17 22:26:37 -0800

    feat(mealplanner): style

--- source/components/meal-planner/meal-planner.js
+++ source/components/meal-planner/meal-planner.js
@@ -3,4 +3,10 @@ window.addEventListener("DOMContentLoaded", () => {
   samplemeal.style.backgroundImage =
     "url(../../static/home-page/demorecipe.jpg)";
   samplemeal.style.backgroundSize = "cover";
+  let mealcards = document.querySelectorAll(".meal-card");
+  for (let i = 0; i < 21; i++) {
+    mealcards[i].addEventListener("click", () => {
+      window.prompt("Meal Url", "defaultText");
+    });
+  }
 });
