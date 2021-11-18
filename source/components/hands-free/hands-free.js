54afe7283127c8009f0d3b494090324644092337
DFC <frankdoufc@gmail.com>
2021-11-18 10:22:09 -0800

    bug(handsfree): eslint

--- source/components/hands-free/hands-free.js
+++ source/components/hands-free/hands-free.js
@@ -1,38 +1,56 @@
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("start-button").addEventListener("click", () => {
+    startCount();
+  });
+  document.getElementById("pause-button").addEventListener("click", () => {
+    stopCount();
+  });
+  document.getElementById("resume-button").addEventListener("click", () => {
+    resumeCount();
+  });
+});
+
 var count = 0;
 var temp;
 var timer_is_on = 0;
 
 function timedCount() {
-    setTime()
-    count = count + 1;
-    temp = setTimeout(timedCount, 1000);
+  setTime();
+  count = count + 1;
+  temp = setTimeout(timedCount, 1000);
 }
 
 function startCount() {
-    if (!timer_is_on) {
-        timer_is_on = 1;
-        timedCount();
-    }
+  if (!timer_is_on) {
+    timer_is_on = 1;
+    timedCount();
+  }
 }
 
 function stopCount() {
-    clearTimeout(temp);
-    timer_is_on = 0;
+  clearTimeout(temp);
+  timer_is_on = 0;
 }
 
 function resumeCount() {
-    count = 0;
-    setTime()
-    stopCount()
-    
+  stopCount();
+  count = 0;
+  setTime();
 }
 
 function setTime() {
-    let hour = parseInt(count / 3600)
-    let minute = parseInt(count / 60);
-    let second = parseInt(count % 60)
-    if (hour < 10) { hour = "0" + hour }
-    if (minute < 10) { minute = "0" + minute }
-    if (second < 10) { second = "0" + second }
-    document.getElementById("timer-display").innerText = hour + ":" + minute + ":" + second;
+  let hour = parseInt(count / 3600);
+  let minute = parseInt(count / 60);
+  let second = parseInt(count % 60);
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+  document.getElementById("timer-display").innerText =
+    hour + ":" + minute + ":" + second;
 }
