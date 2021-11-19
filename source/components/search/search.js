07a1a9f5c862c7e2876ed9568584c77be9a208e1
DFC <frankdoufc@gmail.com>
2021-11-18 23:26:57 -0800

    feat(search): add search page

---
+++ source/components/search/search.js
@@ -0,0 +1,17 @@
+class Search extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  async connectedCallback() {
+    let elementContent = await fetch("components/search/search.html");
+    let elementContentText = await elementContent.text();
+    this.shadowRoot.innerHTML = elementContentText;
+    this.setupElement();
+  }
+
+  setupElement() {}
+}
+
+customElements.define("search", Search);
