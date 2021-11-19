bafb5330420d1b87097afcab0248cc546de79e30
Justin Butera <jbutera@ucsd.edu>
2021-11-19 13:57:22 -0800

    fix(hands-free): update JS to be inside element class (64)

--- source/components/hands-free/hands-free.js
+++ source/components/hands-free/hands-free.js
@@ -11,116 +11,114 @@ class HandsFree extends HTMLElement {
     this.setupElement();
   }
 
-  setupElement() {}
+  count = 0;
+  temp;
+  timeron = 0;
+  step = 1;
+  // step numbers
+  maxStep = 5;
+
+  setupElement() {
+    this.shadowRoot
+      .getElementById("start-button")
+      .addEventListener("click", () => {
+        this.startCount();
+      });
+    this.shadowRoot
+      .getElementById("pause-button")
+      .addEventListener("click", () => {
+        this.stopCount();
+      });
+    this.shadowRoot
+      .getElementById("resume-button")
+      .addEventListener("click", () => {
+        this.resumeCount();
+      });
+    this.shadowRoot
+      .getElementById("next-button")
+      .addEventListener("click", () => {
+        this.nextStep();
+      });
+    this.shadowRoot
+      .getElementById("back-button")
+      .addEventListener("click", () => {
+        this.backStep();
+      });
+    this.hideBackButton();
+  }
+
+  timedCount() {
+    this.setTime();
+    this.count = this.count + 1;
+    this.temp = setTimeout(this.timedCount.bind(this), 1000);
+  }
+
+  startCount() {
+    if (!this.timeron) {
+      this.timeron = 1;
+      this.timedCount();
+    }
+  }
+
+  stopCount() {
+    clearTimeout(this.temp);
+    this.timeron = 0;
+  }
+
+  resumeCount() {
+    this.stopCount();
+    this.count = 0;
+    this.setTime();
+  }
+
+  setTime() {
+    let hour = parseInt(this.count / 3600);
+    let minute = parseInt(this.count / 60);
+    let second = parseInt(this.count % 60);
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    if (second < 10) {
+      second = "0" + second;
+    }
+    this.shadowRoot.getElementById("timer-display").innerText =
+      hour + ":" + minute + ":" + second;
+  }
+
+  nextStep() {
+    if (++this.step == this.maxStep) {
+      this.hideNextButton();
+    }
+    this.showBackButton();
+    this.shadowRoot.getElementById("number").innerText = this.step;
+  }
+
+  backStep() {
+    if (--this.step == 1) {
+      this.hideBackButton();
+    }
+    this.showNextButton();
+    this.shadowRoot.getElementById("number").innerText = this.step;
+  }
+
+  showBackButton() {
+    this.shadowRoot.getElementById("back-button").style.visibility = "visible";
+  }
+
+  hideBackButton() {
+    this.shadowRoot.getElementById("back-button").style.visibility = "hidden";
+  }
+
+  showNextButton() {
+    this.shadowRoot.getElementById("next-button").style.visibility = "visible";
+  }
+
+  hideNextButton() {
+    this.shadowRoot.getElementById("next-button").style.visibility = "hidden";
+  }
 }
 
 customElements.define("hands-free", HandsFree);
-
-// move below codes into class
-var count = 0;
-var temp;
-var timeron = 0;
-var step = 1;
-// step numbers
-var maxStep = 5;
-
-window.addEventListener("DOMContentLoaded", () => {
-  this.shadowRoot
-    .getElementById("start-button")
-    .addEventListener("click", () => {
-      startCount();
-    });
-  this.shadowRoot
-    .getElementById("pause-button")
-    .addEventListener("click", () => {
-      stopCount();
-    });
-  this.shadowRoot
-    .getElementById("resume-button")
-    .addEventListener("click", () => {
-      resumeCount();
-    });
-  this.shadowRoot
-    .getElementById("next-button")
-    .addEventListener("click", () => {
-      nextStep();
-    });
-  this.shadowRoot
-    .getElementById("back-button")
-    .addEventListener("click", () => {
-      backStep();
-    });
-  hidebackButton();
-});
-
-function timedCount() {
-  setTime();
-  count = count + 1;
-  temp = setTimeout(timedCount, 1000);
-}
-
-function startCount() {
-  if (!timeron) {
-    timeron = 1;
-    timedCount();
-  }
-}
-
-function stopCount() {
-  clearTimeout(temp);
-  timeron = 0;
-}
-
-function resumeCount() {
-  stopCount();
-  count = 0;
-  setTime();
-}
-
-function setTime() {
-  let hour = parseInt(count / 3600);
-  let minute = parseInt(count / 60);
-  let second = parseInt(count % 60);
-  if (hour < 10) {
-    hour = "0" + hour;
-  }
-  if (minute < 10) {
-    minute = "0" + minute;
-  }
-  if (second < 10) {
-    second = "0" + second;
-  }
-  this.shadowRoot.getElementById("timer-display").innerText =
-    hour + ":" + minute + ":" + second;
-}
-
-function nextStep() {
-  if (++step == maxStep) {
-    hideNextButton();
-  }
-  showbackButton();
-  this.shadowRoot.getElementById("number").innerText = step;
-}
-
-function backStep() {
-  if (--step == 1) {
-    hidebackButton();
-  }
-  showNextButton();
-  this.shadowRoot.getElementById("number").innerText = step;
-}
-
-function showbackButton() {
-  this.shadowRoot.getElementById("back-button").style.visibility = "visible";
-}
-
-function hidebackButton() {
-  this.shadowRoot.getElementById("back-button").style.visibility = "hidden";
-}
-function showNextButton() {
-  this.shadowRoot.getElementById("next-button").style.visibility = "visible";
-}
-
-function hideNextButton() {
-  this.shadowRoot.getElementById("next-button").style.visibility = "hidden";
-}
