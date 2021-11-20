b78a0a6b0facb7ce9eb368bbf4febe71257a33f8
Baha Keskin <bmkeskin@ucsd.edu>
2021-11-20 14:03:00 -0800

    feat(pipeline): add comments to make sure jsdocs works

--- source/core/routing/router.js
+++ source/core/routing/router.js
@@ -1,8 +1,14 @@
 module.exports = { getRoutefromUrl };
 
-// Route patterns for the different pages of the site.
-// Each route pattern is the URL with all parameters replaced with "_".
-// Key is name of route, value is object with component name and URL.
+/** Router module.
+ * @module core/routing/router
+ */
+
+/**
+ * The router patterns for the different pages of the site.
+ * Each route pattern is the URL with all parameters replaced with "_".
+ * Key is name of component (route), value is object with component name and URL.
+ */
 const routePatterns = {
   "home-page": {
     component: "home-page",
