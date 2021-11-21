5acea80dd8d9e181701a84fef5102bc9b80d951f
Baha Keskin <bmkeskin@ucsd.edu>
2021-11-21 10:59:42 -0800

    feat(pipeline): pulled out unit-tests from source

--- unit-tests/router.test.js
+++ unit-tests/router.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const router = require("../core/routing/router.js");
+const router = require("../source/core/routing/router.js");
 
 test("getRoutefromUrl: testing home-page", () => {
   expect(router.getRoutefromUrl("")).toBe("home-page");
