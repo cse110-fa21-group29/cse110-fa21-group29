1c260a86775012767fee770e8f075d08a6d8c5ea
Baha Keskin <bmkeskin@ucsd.edu>
2021-11-21 15:21:11 -0800

    feat(book): adding book file

--- source/ex-books.js
+++ source/ex-books.js
@@ -4,6 +4,4 @@
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
  */
-function Book(title, author) {
-  // function();
-}
+function Book(title, author) {}
