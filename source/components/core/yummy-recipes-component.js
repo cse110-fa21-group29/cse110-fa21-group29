3e3eb5fca64d858aec704edaec6a81321bab23be
Justin Butera <jbutera@ucsd.edu>
2021-11-27 02:06:49 -0800

    feat(core): add generic yummy recipes component

---
+++ source/components/core/yummy-recipes-component.js
@@ -0,0 +1,45 @@
+export class YummyRecipesComponent extends HTMLElement {
+  /**
+   * Constructor to set up shadow DOM for the component.
+   */
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  /**
+   * Sets the routeParams instance variable to an array of route parameters.
+   * @param {number[]} params - Array of route parameters.
+   */
+  set params(params) {
+    this.routeParams = params;
+  }
+
+  /**
+   * Sets the routeName instance variable to the route's name.
+   * @param {string} route - The route name.
+   */
+  set route(route) {
+    this.routeName = route;
+  }
+
+  /**
+   * Fires when this component is inserted into the DOM.
+   *
+   * @async
+   */
+  async connectedCallback() {
+    const elementContent = await fetch(this.htmlPath);
+    const elementContentText = await elementContent.text();
+
+    this.shadowRoot.innerHTML = elementContentText;
+    this.setupElement();
+  }
+
+  /**
+   * Setup code for this component
+   *
+   * @async
+   */
+  async setupElement() {}
+}
