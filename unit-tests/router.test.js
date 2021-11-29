4cba52b10f33a093885f973454976b405256a9c3
Baha Keskin <bmkeskin@ucsd.edu>
2021-11-29 13:17:29 -0800

    test(pipeline): add comments for each function for routing

--- unit-tests/router.test.js
+++ unit-tests/router.test.js
@@ -3,6 +3,22 @@
  */
 const router = require("../source/core/routing/router.js");
 
+/**
+ * All navigateFromUrl Unit Tests
+ */
+
+/**
+ * All getUrlFromRoute Unit Tests
+ */
+
+/**
+ * All getRoutefromUrl Unit Tests
+ */
+
 test("getRoutefromUrl: testing home-page", () => {
   expect(router.getRoutefromUrl("")).toBe("home-page");
 });
+
+/**
+ * All getParamsFromUrl Unit Tests
+ */
