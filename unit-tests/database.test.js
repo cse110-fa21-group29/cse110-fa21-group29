7aaa99a3d8496ecdadbc318853d3b0556c44b6fe
Solowyhn <47937357+Solowyhn@users.noreply.github.com>
2021-11-29 23:56:36 -0800

    test(database): add frame, run would cause errors, unresolved

--- unit-tests/database.test.js
+++ unit-tests/database.test.js
@@ -1,9 +1,40 @@
 /**
  * @jest-environment jsdom
  */
-
 import { Database } from "../source/core/database/database.js";
 
-test('nothing', () => {
-    expect(1+1).toBe(2);
+/**
+ * getRecipes, return recipe
+ */
+describe('testing getRecipes', () => {
+
 });
+
+/**
+ * pushRecipe, return recipe.length
+ */
+describe('testing pushRecipe', () => {
+
+});
+
+/**
+ * updateRecipe w/ no return
+ */
+describe('testing upadteRecipe', () => {
+
+})
+
+/**
+ * writeData w/ no return
+ */
+describe('testing writeData', () => {
+
+});
+
+/**
+ * deleteRecipe w/ no return
+ */
+describe('testing deleteRecipe', () => {
+
+});
+
