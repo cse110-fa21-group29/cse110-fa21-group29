fb0cd4238bc0521de7dffcb0612d33cf67e771e2
Baha Keskin <bmkeskin@ucsd.edu>
2021-11-30 20:16:16 -0800

    test(pipeline): add e2e for recipe-details page

--- testing/e2e-tests/router.test.js
+++ testing/e2e-tests/router.test.js
@@ -5,10 +5,30 @@
 describe('E2E Testing loading web components and navigating pages', () => {
     beforeAll(async () => {
         await page.goto('https://dev.yummyrecipesapp.com/');
+        await new Promise((r) => setTimeout(r, 2000));
     });
 
+    // Test to see we land on home page
     it('Check we land on home page', async () => {
         const homePage = await page.$$('#content > home-page');
         expect(homePage.length).toBe(1);
+    }, 2500);
+
+    // Test to see if we go to recipe-details page
+    it('Check if we move to recipe-detail page', async () => {
+        
+        // get the shadow root of the home page
+        let homePage = await page.waitForSelector('home-page');
+        let shadowRoot = await homePage.getProperty('shadowRoot');
+        
+        // click on a recipe card
+        let recipe = await shadowRoot.$('common-recipe-card');
+        recipe.click();
+
+        // wait for the page to load then get the recipe-details page
+        await new Promise((r) => setTimeout(r, 1000));
+        let recipeDetailsPage = await page.$$('recipe-details');
+
+        expect(recipeDetailsPage.length).toBe(1);
     }, 2500);
 });
