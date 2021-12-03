aa1a09a267306db020816e007b869e9bf4eb93df
Edbert Dai <eddai@ucsd.edu>
2021-12-02 20:13:38 -0800

    feat(meal-planner): implement adding to meal planner by user link entry

--- source/components/meal-planner/meal-planner.js
+++ source/components/meal-planner/meal-planner.js
@@ -1,3 +1,4 @@
+import { Database } from "/core/database/database.js";
 import { YummyRecipesComponent } from "/components/core/yummy-recipes-component.js";
 
 class MealPlanner extends YummyRecipesComponent {
@@ -6,16 +7,75 @@ class MealPlanner extends YummyRecipesComponent {
     this.htmlPath = "components/meal-planner/meal-planner.html";
   }
 
+  /**
+   * Sets up meal card functions
+   */
   setupElement() {
-    let samplemeal = this.shadowRoot.getElementById("sample-meal");
-    samplemeal.style.backgroundImage = "url(static/common/demorecipe.jpg)";
-    samplemeal.style.backgroundSize = "cover";
-    let mealcards = this.shadowRoot.querySelectorAll(".meal-card");
+    const mealCards = this.shadowRoot.querySelectorAll(".meal-card");
+
     for (let i = 0; i < 21; i++) {
-      mealcards[i].addEventListener("click", () => {
-        window.prompt("Meal Url", "defaultText");
+      mealCards[i].addEventListener("click", () => {
+        // Function that handles meal card logic
+        this.createRecipeCard(mealCards[i]);
       });
     }
+  }
+
+  /**
+   * Asks for user input of recipe link and checks if valid. If valid, create
+   * recipe card and append to meal card.
+   *
+   * @param {Object} mealCard - Meal card in planner to append to.
+   */
+  async createRecipeCard(mealCard) {
+    // Get user entry
+    const prompt = window.prompt("Enter recipe link", "");
+
+    // Do not do anything if prompt canceled
+    if (prompt == undefined) {
+      return;
+    }
+
+    // Reset meal card if empty entry
+    if (prompt == "") {
+      mealCard.innerHTML = "";
+      return;
+    }
+
+    // Split input and grab last split
+    const url = prompt.split("/");
+    const index = parseInt(url[url.length - 1]);
+
+    // Object to hold recipe if found
+    let recipe = {};
+
+    // Check if split contains "recipes" followed by number
+    if (url[url.length - 2] == "recipes" && isNaN(index) === false) {
+      // Try to get recipe from database
+      const db = new Database();
+      recipe = await db.getRecipe(index);
+
+      // If recipe does not exist notify user
+      if (
+        recipe &&
+        Object.keys(recipe).length === 0 &&
+        Object.getPrototypeOf(recipe) === Object.prototype
+      ) {
+        alert("Not a valid recipe link");
+        return;
+      }
+    } else {
+      alert("Not a valid recipe link");
+      return;
+    }
+
+    // Generate recipe card
+    const card = document.createElement("common-recipe-card");
+    card.recipeData = recipe;
+
+    // Clear out meal card then append
+    mealCard.innerHTML = "";
+    mealCard.append(card);
   }
 }
 
