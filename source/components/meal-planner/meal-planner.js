54170c847088d150835910327a7dfcdf5977da90
Edbert Dai <eddai@ucsd.edu>
2021-12-04 23:47:50 -0800

    feat(meal-planner): add unique links for each meal planner

--- source/components/meal-planner/meal-planner.js
+++ source/components/meal-planner/meal-planner.js
@@ -10,18 +10,54 @@ class MealPlanner extends YummyRecipesComponent {
 
   /**
    * Sets up meal planner functions
+   *
+   * @async
    */
-  setupElement() {
+  async setupElement() {
+    // Get recipe indexes from URL and route to homepage if failed
+    const indexes = this.getParams();
+
+    if (indexes === undefined) {
+      const routerEvent = new CustomEvent("router-navigate", {
+        detail: {
+          route: "home-page",
+          params: [],
+        },
+        bubbles: true,
+        composed: true,
+      });
+      this.dispatchEvent(routerEvent);
+      return;
+    }
+
     // Select all cells
     const mealCards = this.shadowRoot.querySelectorAll(".meal-card");
 
-    // Add cell functions
+    // Add cell functions and recipe cards if applicable
     for (let i = 0; i < 21; i++) {
-      // Append add button to cell
+      // Add button for empty cells
       const addButton = document.createElement("div");
 
       addButton.className = "meal-card-add";
-      mealCards[i].append(addButton);
+
+      // Check if URL index is not -1
+      if (indexes[i] !== -1) {
+        // Attempt to get recipe from index
+        const db = new Database();
+        const recipe = await db.getRecipe(indexes[i]);
+
+        // Check if recipe is not undefined
+        if (recipe !== undefined) {
+          // Append recipe card to cell
+          this.addRecipeToCell(mealCards[i], indexes[i]);
+        } else {
+          // Append add button to cell
+          mealCards[i].append(addButton);
+        }
+      } else {
+        // Append add button to cell
+        mealCards[i].append(addButton);
+      }
 
       // Bring up search sidebar when add button clicked
       addButton.addEventListener("click", () => {
@@ -41,8 +77,14 @@ class MealPlanner extends YummyRecipesComponent {
 
       // Drop listener
       mealCards[i].addEventListener("drop", (event) => {
+        // Get recipe index from drag data
         const index = event.dataTransfer.getData("text/plain");
+
+        // Add recipe card
         this.addRecipeToCell(mealCards[i], index);
+
+        // Change URL
+        this.setUrl(i, index);
       });
     }
 
@@ -61,6 +103,11 @@ class MealPlanner extends YummyRecipesComponent {
         this.search(query);
         event.preventDefault();
       });
+
+    // Reload page when browser navigation buttons used
+    window.onpopstate = function (e) {
+      location.reload();
+    };
   }
 
   /**
@@ -118,6 +165,58 @@ class MealPlanner extends YummyRecipesComponent {
     // Clear out any existing cards
     mealCard.innerHTML = "";
     mealCard.append(card);
+  }
+
+  /**
+   * Adds meal planner recipe card to cell.
+   *
+   * @async
+   * @param {Object} mealCard - Cell to append card to.
+   * @param {number} index - Index of recipe object in database.
+   * @returns {Array|undefined} Array of indexes or undefined if not all numbers.
+   */
+  getParams() {
+    // Get params
+    const paramString = window.location.href.split("?")[1];
+    const searchParams = new URLSearchParams(paramString);
+
+    // Check if "ids" exists as a query string parameter
+    if (searchParams.has("ids")) {
+      const ids = searchParams.get("ids").split(",").map(Number);
+
+      // Check if there are exactly 21 numbers
+      if (ids.length == 21 && ids.every((val) => !isNaN(val))) {
+        return ids;
+      }
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Replaces URL with modified "ids" query string parameter.
+   *
+   * @param {number} paramIndex - The id in the parameter to replace.
+   * @param {number} recipeIndex - The replacement value of the id.
+   */
+  setUrl(paramIndex, recipeIndex) {
+    // Get params
+    const paramString = window.location.href.split("?")[1];
+    const searchParams = new URLSearchParams(paramString);
+
+    // Replace id
+    const ids = searchParams.get("ids").split(",");
+    ids[paramIndex] = recipeIndex;
+
+    const newIds = ids.join(",");
+
+    // Get URL and remove query string
+    const url = window.location.href;
+    let newUrl = url.slice(0, url.indexOf("?"));
+    newUrl += "?ids=" + newIds;
+
+    // Push new URL
+    history.pushState({}, "", newUrl);
   }
 }
 
