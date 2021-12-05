ac64936afc347c61292f09e0830610b163ff2916
Baha Keskin <bmkeskin@ucsd.edu>
2021-12-04 17:05:42 -0800

    test(pipeline): add e2e test hands-free mode page

--- testing/e2e-tests/router.test.js
+++ testing/e2e-tests/router.test.js
@@ -12,7 +12,7 @@ describe('E2E Testing loading web components and navigating pages', () => {
      * Test to see we land on home page
      */
     it('Land on home page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // get the home page
         const homePage = await page.$$('#content > home-page');
@@ -22,21 +22,18 @@ describe('E2E Testing loading web components and navigating pages', () => {
 
     /**
      * @summary Recipe Details Page
-     * Test to see if we go to recipe details page
+     * Test to see if we go to recipe details page from home page
      */
     it('Moving to recipe detail page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
-        // get the shadow root of the home page
-        const homePage = await page.waitForSelector('home-page');
+        // click on a recipe card on the home page
+        const homePage = await page.$('home-page');
         const shadowRoot = await homePage.getProperty('shadowRoot');
-        
-        // click on a recipe card
         const recipe = await shadowRoot.$('common-recipe-card');
         recipe.click();
 
-        // wait for the page to load then get the recipe-details page
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         const recipeDetailsPage = await page.$$('#content > recipe-details');
 
@@ -48,15 +45,15 @@ describe('E2E Testing loading web components and navigating pages', () => {
      * Test to see if we go to the existing contribute page
      */
      it('Moving to existing contribute page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // click on the edit button
         const recipeDetails = await page.$('#content > recipe-details');
         const shadowRoot = await recipeDetails.getProperty('shadowRoot');
-        const homePageButton = await shadowRoot.$('#edit-button');
-        homePageButton.click();
+        const editButton = await shadowRoot.$('#edit-button');
+        editButton.click();
 
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // get the contribute page
         const contributePage = await page.$$('#content > recipe-contribute');
@@ -69,7 +66,7 @@ describe('E2E Testing loading web components and navigating pages', () => {
      * Test to see if we go back to the home page
      */
      it('Moving back to home page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // click on the home page nav bar button
         const navBar = await page.$('body > header > common-nav-bar');
@@ -77,7 +74,7 @@ describe('E2E Testing loading web components and navigating pages', () => {
         const homePageButton = await shadowRoot.$('#home-page-link');
         homePageButton.click();
 
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
         
         // get the home page
         const homePage = await page.$$('#content > home-page');
@@ -90,15 +87,15 @@ describe('E2E Testing loading web components and navigating pages', () => {
      * Test to see if we go to the new contribute page
      */
      it('Moving to new contribute page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // click on the contribute nav bar button
         const navBar = await page.$('body > header > common-nav-bar');
         const shadowRoot = await navBar.getProperty('shadowRoot');
-        const homePageButton = await shadowRoot.$('#nav-bar > a:nth-child(4)');
-        homePageButton.click();
+        const contributePageButton = await shadowRoot.$('#nav-bar > a:nth-child(4)');
+        contributePageButton.click();
 
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // get the contribute page
         const contributePage = await page.$$('#content > recipe-contribute');
@@ -111,20 +108,20 @@ describe('E2E Testing loading web components and navigating pages', () => {
      * Test to see if we go to the meal planner page
      */
      it('Moving to meal planner page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // click on the meal planner nav bar button
         const navBar = await page.$('body > header > common-nav-bar');
         const shadowRoot = await navBar.getProperty('shadowRoot');
-        const homePageButton = await shadowRoot.$('#nav-bar > a:nth-child(5)');
-        homePageButton.click();
+        const mealPlannerButton = await shadowRoot.$('#nav-bar > a:nth-child(5)');
+        mealPlannerButton.click();
 
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // get the meal planner page
-        const contributePage = await page.$$('#content > meal-planner');
+        const mealPlannerPage = await page.$$('#content > meal-planner');
 
-        expect(contributePage.length).toBe(1);
+        expect(mealPlannerPage.length).toBe(1);
     }, 7500);
 
     /**
@@ -132,20 +129,20 @@ describe('E2E Testing loading web components and navigating pages', () => {
      * Test to see if we go to the about us page
      */
      it('Moving to about us page', async () => {
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // click on the about us nav bar button
         const navBar = await page.$('body > header > common-nav-bar');
         const shadowRoot = await navBar.getProperty('shadowRoot');
-        const homePageButton = await shadowRoot.$('#nav-bar > a:nth-child(6)');
-        homePageButton.click();
+        const aboutUsButton = await shadowRoot.$('#nav-bar > a:nth-child(6)');
+        aboutUsButton.click();
 
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // get the about us page
-        const contributePage = await page.$$('#content > about-us');
+        const aboutUsPage = await page.$$('#content > about-us');
 
-        expect(contributePage.length).toBe(1);
+        expect(aboutUsPage.length).toBe(1);
     }, 7500);
 
     /**
@@ -158,14 +155,54 @@ describe('E2E Testing loading web components and navigating pages', () => {
         // click on the search nav bar button
         const navBar = await page.$('body > header > common-nav-bar');
         const shadowRoot = await navBar.getProperty('shadowRoot');
-        const homePageButton = await shadowRoot.$('#search-button');
-        homePageButton.click();
+        const searchPageButton = await shadowRoot.$('#search-button');
+        searchPageButton.click();
 
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
 
         // get the search page
-        const contributePage = await page.$$('#content > recipe-search');
+        const searchPage = await page.$$('#content > recipe-search');
 
-        expect(contributePage.length).toBe(1);
+        expect(searchPage.length).toBe(1);
+    }, 7500);
+
+    /**
+     * @summary Recipe Details Page
+     * Test to see if we go to recipe details page from search page
+     */
+     it('Moving to recipe detail page', async () => {
+        await new Promise((r) => setTimeout(r, 2000));
+
+        // click on a recipe card on search page
+        const recipeSearch = await page.$('#content > recipe-search');
+        const shadowRoot = await recipeSearch.getProperty('shadowRoot');
+        const recipeCard = await shadowRoot.$('#recipe-card-grid > common-recipe-card:nth-child(1)');
+        recipeCard.click();
+
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
+
+        const recipeDetailsPage = await page.$$('#content > recipe-details');
+
+        expect(recipeDetailsPage.length).toBe(1);
+    }, 7500);
+
+    /**
+     * @summary Hands-Free Mode Page
+     * Test to see if we go to hands free page
+     */
+     it('Moving to hands-free mode page', async () => {
+        await new Promise((r) => setTimeout(r, 2000));
+
+        // click on the hands-free mode button
+        const recipeDetails = await page.$('#content > recipe-details');
+        const shadowRoot = await recipeDetails.getProperty('shadowRoot');
+        const homePageButton = await shadowRoot.$('#hands-free-button');
+        homePageButton.click();
+
+        await new Promise((r) => setTimeout(r, 2000)); // wait for the page to load
+
+        const handsFreeModePage = await page.$$('#content > hands-free');
+
+        expect(handsFreeModePage.length).toBe(1);
     }, 7500);
 });
