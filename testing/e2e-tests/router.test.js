36aaf3d5d068527ee1531ffe05ec556f095fec8a
Baha Keskin <bmkeskin@ucsd.edu>
2021-12-04 16:45:44 -0800

    test(pipeline): added comments to e2e tests

--- testing/e2e-tests/router.test.js
+++ testing/e2e-tests/router.test.js
@@ -5,23 +5,28 @@
 describe('E2E Testing loading web components and navigating pages', () => {
     beforeAll(async () => {
         await page.goto('https://dev.yummyrecipesapp.com/');
-        await new Promise((r) => setTimeout(r, 2000));
     });
 
     /**
+     * @summary Land Home Page
      * Test to see we land on home page
      */
-    it('Check if we land on home page', async () => {
+    it('Land on home page', async () => {
+        await new Promise((r) => setTimeout(r, 2000));
+
         // get the home page
         const homePage = await page.$$('#content > home-page');
 
         expect(homePage.length).toBe(1);
-    }, 5000);
+    }, 7500);
 
     /**
-     * Test to see if we go to recipe-details page
+     * @summary Recipe Details Page
+     * Test to see if we go to recipe details page
      */
-    it('Check if we move to recipe-detail page', async () => {
+    it('Moving to recipe detail page', async () => {
+        await new Promise((r) => setTimeout(r, 2000));
+
         // get the shadow root of the home page
         const homePage = await page.waitForSelector('home-page');
         const shadowRoot = await homePage.getProperty('shadowRoot');
@@ -32,8 +37,9 @@ describe('E2E Testing loading web components and navigating pages', () => {
 
         // wait for the page to load then get the recipe-details page
         await new Promise((r) => setTimeout(r, 2000));
-        const recipeDetailsPage = await page.$$('recipe-details');
+
+        const recipeDetailsPage = await page.$$('#content > recipe-details');
 
         expect(recipeDetailsPage.length).toBe(1);
-    }, 5000);
+    }, 7500);
 });
