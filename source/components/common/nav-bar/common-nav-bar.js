06b183d0d09d8134ba5b6ff0de74b5a882d8755d
Baha Keskin <bmkeskin@ucsd.edu>
2021-12-06 13:59:29 -0800

    feat(nav-bar): remove categories, search bar. removed js. made logo clickable

--- source/components/common/nav-bar/common-nav-bar.js
+++ source/components/common/nav-bar/common-nav-bar.js
@@ -5,37 +5,6 @@ class CommonNavBar extends YummyRecipesComponent {
     super();
     this.htmlPath = "components/common/nav-bar/common-nav-bar.html";
   }
-
-  /**
-   * Initializes the nav bar component.
-   */
-  setupElement() {
-    this.shadowRoot
-      .getElementById("search-button")
-      .addEventListener("click", (event) => {
-        event.preventDefault();
-
-        // Route to search page
-        const routerEvent = new CustomEvent("router-navigate", {
-          detail: {
-            route: "recipe-search",
-            params: [],
-            searchParams: {
-              query: this.shadowRoot.getElementById("search-input").value,
-              glutenFree: true,
-              healthy: true,
-              highProtein: true,
-              vegan: true,
-              vegetarian: true,
-            },
-          },
-          bubbles: true,
-          composed: true,
-        });
-
-        document.dispatchEvent(routerEvent);
-      });
-  }
 }
 
 customElements.define("common-nav-bar", CommonNavBar);
